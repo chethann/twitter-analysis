e9f539a43d9dfa9141973ae61af38a5ee9672911
Chethan N <chethan.n@myntra.com>
2016-04-16 15:23:04 +0530

    Initial setup commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+// This responds with "Hello World" on the homepage
+app.get('/', function (req, res) {
+   console.log("Got a GET request for the homepage");
+   res.send('Hello GET');
+})
+
+
+var server = app.listen(8081, function () {
+})
